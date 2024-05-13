6090241b4588583e6a0546e1f2bc466749f38868
Andre Paiement <andrep2003@hotmail.com>
2024-05-13 17:56:36 -0400

    display and commit images

---
+++ frontend/src/components/Cards.js
@@ -0,0 +1,22 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import ImageCard from './ImageCard';
+
+const cards = ({ images, setImages }) => {
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id));
+  };
+
+  return (
+    <Container className="mt-4">
+      <Row xs={1} md={2} lg={3}>
+        {images.map((image, i) => (
+          <Col key={i} className="pb-3">
+            <ImageCard image={image} deleteImage={handleDeleteImage} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default cards;
